93e9b81acc8f14b3055e0e81e6c5af7b531229f3
Junya <ueda.the.tallest.pilot@gmail.com>
2022-02-13 16:58:25 +0930

    aboutページ PC完成

--- page-about.php
+++ page-about.php
@@ -1,3 +1,105 @@
 <?php get_header(); ?>
 
+<h2 class="page-title page-title--about">私について</h2>
+
+<section class="introduction">
+    <div class="inner introduction__inner">
+        <h3 class="page-section-title page-section-title--about introduction__title">自己紹介</h3>
+        <div class="introduction__container">
+            <p class="introduction__job-title">Web Developer</p>
+            <h4 class="introduction__name">Junya Ueda</h4>
+            <p class="introduction__text">
+                こんにちは。<br>
+                フリーランスとしてWeb制作をしてます、上田隼也（ウエダジュンヤ）と申します。<br>
+                「聞き上手・説明上手・誠実」を心がけ、あなたのビジネスのお役に立てるWebサイトの構築をいたします。<br>
+                性格は特に真面目で、人の手助けをしてあげたいという気持ちが強い傾向があります。<br>
+                そのおかげもあって、一度ご依頼いただいた方から再度ご依頼を受けたり、サイト以外の事でもご相談を頂いたりすることが多いのが自慢です。
+            </p>
+            <h5 class="certification__title">保有資格</h5>
+            <div class="certification__container">
+                <span class="certification__item">基本情報技術者</span>
+                <span class="certification__item">調理師免許</span>
+                <span class="certification__item">自家用操縦士（滑空機上級）</span>
+                <span class="certification__item">操縦教育証明</span>
+                <span class="certification__item">TOEIC 810点</span>
+                <span class="certification__item">IELTS 6.0</span>
+            </div>
+            <!-- /.certification-container -->
+            <figure class="introduction__image"><img src="<?php echo get_template_directory_uri(); ?>/images/about/introduction-image.jpg" alt=""></figure>
+        </div>
+        <!-- /.introduction__container -->
+    </div>
+    <!-- /.inner introduction__inner -->
+</section>
+<!-- /.introduction -->
+
+
+<section class="history">
+    <div class="inner history__inner">
+        <h3 class="page-section-title page-section-title--about history__title">経歴</h3>
+        <div class="history__table">
+            <span class="history__year">2007年</span>
+            <h4 class="history__heading">早稲田大学第一文学部卒業</h4>
+            <p class="history__body">日本史学を専攻。部活では体育会航空部に所属し、自家用操縦士（上級滑空機）と教官の資格を獲得。「相談しやすい先輩」との評価を得る。</p>
+            <span class="history__year">2007年~</span>
+            <h4 class="history__heading">大手SIerに就職</h4>
+            <p class="history__body">プログラムに関することを叩き込まれる。主に上流工程と、お客様とのコミュニケーション方法を学ぶ。</p>
+            <span class="history__year">2009年~</span>
+            <h4 class="history__heading">ワーキングホリデーで渡豪</h4>
+            <p class="history__body">2年間の海外経験を通じ、多文化への理解を習得。多面的なものの見方や、相手の立場に立って物を考える癖を身に着ける。</p>
+            <span class="history__year">2011年~</span>
+            <h4 class="history__heading">板前修業</h4>
+            <p class="history__body">移住を目指し、ビザ取得に有利な料理人の道に入る。昔ながらの職人の世界に揉まれ、根気と根性を叩き込まれる。</p>
+            <span class="history__year">2014年~</span>
+            <h4 class="history__heading">再度渡豪し、副料理長に</h4>
+            <p class="history__body">責任感を買われ、副料理長に抜擢される。どのように説明すれば分かりやすいかを常に意識しながら、新人や後輩の教育も行う。</p>
+            <span class="history__year">2020年~</span>
+            <h4 class="history__heading">フリーランスとして活動</h4>
+            <p class="history__body">持ち前の自走力を発揮し、全て単独でWeb制作を学ぶ。一度ご依頼いただいたお客様には信用を頂き、プロジェクト後も継続してお仕事を頂くことが多い。</p>
+        </div>
+        <!-- /.history__table -->
+    </div>
+    <!-- /.inner history__inner -->
+</section>
+<!-- /.history -->
+
+<section class="skills section-blue">
+    <div class="inner skills__inner">
+        <h3 class="skills__title">対応範囲・スキル感について</h3>
+        <ul class="skills__list">
+            <li class="skills__item">
+                <figure class="skills__icon"><img src="<?php echo get_template_directory_uri(); ?>/images/about/html.png" alt=""></figure>
+                <div class="skills__text-area">
+                    <h4 class="skills__lang">HTML5(マークアップ)</h4>
+                    <p class="skills__body">ココにはHTML(マークアップ)をしていく上で、気を付けている事や記述方法などを具体的に書いて下さい。ココにはHTML(マークアップ)をしていく上で、気を付けている事や記述方法などを具体的に書いて下さい。ココにはHTML(マークアップ)をしていく上で、気を付けている事や記述方法などを具体的に書いて下さい。ココにはHTML(マークアップ)をしていく上で、気を付けている事や記述方法などを具体的に書いて下さい。ココにはHTML(マークアップ)をしていく上で、気を付けている事や記述方法などを具体的に書いて下さい。ココにはHTML(マークアップ)をしていく上で、気を付けている事や記述方法などを具体的に書いて下さい。</p>
+                </div>
+            </li>
+            <li class="skills__item">
+                <figure class="skills__icon"><img src="<?php echo get_template_directory_uri(); ?>/images/about/css.png" alt=""></figure>
+                <div class="skills__text-area">
+                    <h4 class="skills__lang">CSS(Lib Sass / Dart Sass)</h4>
+                    <p class="skills__body">ココにはCSSを記述していく上で、気を付けている事や記述方法などを具体的に書いて下さい。ココにはCSSを記述していく上で、気を付けている事や記述方法などを具体的に書いて下さい。ココにはCSSを記述していく上で、気を付けている事や記述方法などを具体的に書いて下さい。ココにはCSSを記述していく上で、気を付けている事や記述方法などを具体的に書いて下さい。ココにはCSSを記述していく上で、気を付けている事や記述方法などを具体的に書いて下さい。ココにはCSSを記述していく上で、気を付けている事や記述方法などを具体的に書いて下さい。ココにはCSSを記述していく上で、気を付けている事や記述方法などを具体的に書いて下さい。ココにはCSSを記述していく上で、気を付けている事や記述方法などを具体的に書いて下さい。</p>
+                </div>
+            </li>
+            <li class="skills__item">
+                <figure class="skills__icon"><img src="<?php echo get_template_directory_uri(); ?>/images/about/js.png" alt=""></figure>
+                <div class="skills__text-area">
+                    <h4 class="skills__lang">JavaScript</h4>
+                    <p class="skills__body">ココにはHTML(マークアップ)をしていく上で、気を付けている事や記述方法などを
+具体的に書いて下さい。ココにはHTML(マークアップ)をしていく上で、気を付けている事や記述方法などを具体的に書いて下さい。ココにはHTML(マークアップ)をしていく上で、気を付けている事や記述方法などを具体的に書いて下さい。ココにはHTML(マークアップ)をしていく上で、気を付けている事や記述方法などを具体的に書いて下さい。ココにはHTML(マークアップ)をしていく上で、気を付けている事や記述方法などを具体的に書いて下さい。ココにはHTML(マークアップ)をしていく上で、気を付けている事や記述方法などを具体的に書いて下さい。</p>
+                </div>
+            </li>
+            <li class="skills__item">
+                <figure class="skills__icon"><img src="<?php echo get_template_directory_uri(); ?>/images/about/php.png" alt=""></figure>
+                <div class="skills__text-area">
+                    <h4 class="skills__lang">PHP(WordPress)</h4>
+                    <p class="skills__body">ココにはPHP・WordPressで行える範囲、気を付けている事や記述方法などを具体的に書いて下さい。ココにはPHP・WordPressで行える範囲、気を付けている事や記述方法などを具体的に書いて下さい。ココにはPHP・WordPressで行える範囲、気を付けている事や記述方法などを具体的に書いて下さい。ココにはPHP・WordPressで行える範囲、気を付けている事や記述方法などを具体的に書いて下さい。ココにはPHP・WordPressで行える範囲、気を付けている事や記述方法などを具体的に書いて下さい。ココにはPHP・WordPressで行える範囲、気を付けている事や記述方法などを具体的に書いて下さい。ココにはPHP・WordPressで行える範囲、気を付けている事や記述方法などを具体的に書いて下さい。ココにはPHP・WordPressで行える範囲、気を付けている事や記述方法などを具体的に書いて下さい。</p>
+                </div>
+            </li>
+        </ul>
+        <!-- /.skills__list -->
+    </div>
+    <!-- /.inner skills__inner -->
+</section>
+<!-- /.skills section-blue -->
 <?php get_footer(); ?>
