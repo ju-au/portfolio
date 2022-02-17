db009b6196d9babe2123fb84bd88a8690b7fead7
Junya <ueda.the.tallest.pilot@gmail.com>
2022-02-17 17:15:34 +0930

    Enquiryページ PC完成

---
+++ page-enquiry.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<h2 class="page-title page-title--plans">お問い合わせ</h2>
+
+<section class="enquiry">
+    <div class="inner enquiry__inner">
+        <h3 class="page-section-title page-section-title--enquiry enquiry__title">お問い合わせフォーム</h3>
+        <p class="enquiry__text">ご依頼・お見積り・お問い合わせは、以下のフォームからお願いします。</p>
+        <?php echo do_shortcode('[contact-form-7 id="57" title="enquiry"]'); ?>
+    </div>
+    <!-- /.inner enquiry__inner -->
+</section>
+<!-- /.enquiry -->
+
+
+<?php get_footer(); ?>
