496c27b541615cdfcc6eff20d16f993b6360131d
Junya <ueda.the.tallest.pilot@gmail.com>
2022-02-17 17:15:06 +0930

    Enquiryページ PC完成

--- footer.php
+++ footer.php
@@ -3,14 +3,16 @@
 </main>
 
 <footer class="footer">
-    <div class="footer__enquiry" style="background-image: url('<?php echo get_template_directory_uri(); ?>/dist/images/footer.webp');">
-        <h3 class="footer__title">お問い合わせ</h3>
-        <p class="footer__copy">
-            お見積りやご相談を受けてつけています。<br>
-            お気軽にお問い合わせください。
-        </p>
-        <a href="<?php home_url(); ?>/enquiry" class="button footer__button">お問い合わせをする</a>
-    </div>
+    <?php if (!is_page(55)) : ?>
+        <div class="footer__enquiry" style="background-image: url('<?php echo get_template_directory_uri(); ?>/dist/images/footer.webp');">
+            <h3 class="footer__title">お問い合わせ</h3>
+            <p class="footer__copy">
+                お見積りやご相談を受けてつけています。<br>
+                お気軽にお問い合わせください。
+            </p>
+            <a href="<?php home_url(); ?>/enquiry" class="button footer__button">お問い合わせをする</a>
+        </div>
+    <?php endif; ?>
     <!-- /.footer__enquiry -->
     <nav class="footer__nav">
         <ul class="footer__navUl">
@@ -21,9 +23,9 @@
             <li class="footer__navItem"><a href="<?php home_url() ?>/enquiry">お問い合わせ</a></li>
         </ul>
         <!-- /.footer__navUl -->
+        <p class="footer__copyright">&copy;Junya Ueda 2022 All Rights Reserved</p>
     </nav>
     <!-- /.footer__nav -->
-    <p class="footer__copyright">&copy;Junya Ueda 2022 All Rights Reserved</p>
 </footer>
 
 
